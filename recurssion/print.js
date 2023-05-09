b6385da3b3b7c36a8565f03a37a3d7dc54db8415
mostofa kamal <mostofakamal1990@gmail.com>
2023-05-09 07:58:34 +0600

    recursion add

---
+++ recurssion/print.js
@@ -0,0 +1,32 @@
+
+function main() {
+    let n = 5;
+    print(n);
+}
+main();
+
+function print(n) {
+
+    console.log(n);
+
+    print1(n);
+}
+function print1(n) {
+    console.log(n);
+    print2(n);
+}
+function print2(n) {
+    console.log(n);
+    print3(n);
+}
+function print3(n) {
+    console.log(n);
+    print4(n);
+}
+function print4(n) {
+    console.log(n);
+    print5(n);
+}
+function print5(n) {
+    console.log(n);
+}
